736b454934edb18672586783ac4bc8b9f5f8e3b3
The White Hat KE <austinkings30@gmail.com>
2025-02-04 03:01:58 +0300

    Configure puppeteer to run on different environments

--- src/app/api/convert/route.ts
+++ src/app/api/convert/route.ts
@@ -1,22 +1,32 @@
 import { type NextRequest, NextResponse } from "next/server"
-import puppeteer from "puppeteer"
+import puppeteer from "puppeteer-core"
+import chromium from "chrome-aws-lambda";
 
 export async function POST(req: NextRequest) {
   const { html, name } = await req.json()
     if (!html) {
         return NextResponse.json({ error: "HTML content is required" }, { status: 400 })
     }
-    let userName = name;
-    if(!name){
-        userName = "New Resume";
-    }
+    let userName = name || "New_Resume";
+    userName = userName.replace(/\s+/g, "_"); // Replace spaces for filename safety
+
+    const isProd = process.env.VERCEL_ENV === "production";
 
     let browser
     try {
-        browser = await puppeteer.launch({
-            headless: true,
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        if(isProd){
+            browser = await puppeteer.launch({
+                args: chromium.args,
+                defaultViewport: chromium.defaultViewport,
+                executablePath: await chromium.executablePath, // Use the Lambda-compatible Chrome path
+                headless: true,
+            });
+        } else {
+            browser = await puppeteer.launch({
+                headless: true,
+                args: ["--no-sandbox", "--disable-setuid-sandbox"],
             })
+        }
         const page = await browser.newPage()
         // Set the viewport to match A4 size
         await page.setViewport({
@@ -25,13 +35,13 @@ export async function POST(req: NextRequest) {
             deviceScaleFactor: 2, // Increase for better quality
         })
         await page.setContent(html, { waitUntil: "networkidle0" })
-        await page.evaluate((name) => {
+        await page.evaluate((name:string) => {
             document.title = `${name}'s Resume`;
         }, userName);
         const pdf = await page.pdf({ 
             margin: { top: "5mm", right: "0mm", bottom: "5mm", left: "0mm" },
             printBackground: true,
-            format: "A4" 
+            format: "a4" 
         })
         await browser.close()
 
@@ -39,7 +49,7 @@ export async function POST(req: NextRequest) {
         status: 200,
         headers: {
             "Content-Type": "application/pdf",
-            "Content-Disposition": `attachment; filename=${userName} Resume.pdf`,
+            "Content-Disposition": `attachment; filename=${userName}_Resume.pdf`,
         },
         })
     } catch (error) {
