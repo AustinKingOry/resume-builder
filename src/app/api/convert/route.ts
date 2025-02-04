d8ee27a98f8ae710b9895e066c79753dbce4cde5
The White Hat KE <austinkings30@gmail.com>
2025-02-04 03:31:37 +0300

    Default back to using puppeteer package.ust find solution for prod

--- src/app/api/convert/route.ts
+++ src/app/api/convert/route.ts
@@ -1,6 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server"
-import puppeteer from "puppeteer-core"
-import chromium from "chrome-aws-lambda";
+import puppeteer from "puppeteer"
 
 export async function POST(req: NextRequest) {
   const { html, name } = await req.json()
@@ -10,23 +9,13 @@ export async function POST(req: NextRequest) {
     let userName = name || "New_Resume";
     userName = userName.replace(/\s+/g, "_"); // Replace spaces for filename safety
 
-    const isProd = process.env.VERCEL_ENV === "production";
 
     let browser
     try {
-        if(isProd){
-            browser = await puppeteer.launch({
-                args: chromium.args,
-                defaultViewport: chromium.defaultViewport,
-                executablePath: await chromium.executablePath, // Use the Lambda-compatible Chrome path
-                headless: true,
-            });
-        } else {
-            browser = await puppeteer.launch({
-                headless: true,
-                args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            })
-        }
+        browser = await puppeteer.launch({
+            headless: true,
+            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
         const page = await browser.newPage()
         // Set the viewport to match A4 size
         await page.setViewport({
