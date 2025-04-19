1e3a928e0473903b6e8d35f7ec79ca6cf9632b39
The White Hat KE <austinkings30@gmail.com>
2025-04-20 01:27:00 +0300

    Update links in hero section

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { ArrowRight } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export function HeroSection() {
     return (
@@ -9,7 +10,7 @@ export function HeroSection() {
         <div className="container relative z-10 mx-auto px-4 md:px-6">
             <div className="grid gap-12 lg:grid-cols-2 lg:gap-8 items-center">
             <div className="flex flex-col justify-center space-y-4">
-                <div className="inline-block rounded-lg bg-[#10B981]/10 px-3 py-1 text-sm text-[#10B981]">
+                <div className="inline-block rounded-lg bg-[#10B981]/10 px-3 py-1 text-sm text-[#10B981] w-fit">
                 Your Career Toolkit
                 </div>
                 <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-[#1E3A8A]">
@@ -20,11 +21,11 @@ export function HeroSection() {
                 journey.
                 </p>
                 <div className="flex flex-col sm:flex-row gap-4 pt-4">
-                <Button size="lg" className="bg-[#1E3A8A] hover:bg-[#1E3A8A]/90">
-                    Start Building <ArrowRight className="ml-2 h-4 w-4" />
+                <Button size="lg" className="bg-[#1E3A8A] hover:bg-[#1E3A8A]/90" asChild>
+                    <Link href={'/builder'}>Start Building <ArrowRight className="ml-2 h-4 w-4" /></Link>                    
                 </Button>
-                <Button size="lg" variant="outline" className="border-[#1E3A8A] text-[#1E3A8A] hover:bg-[#1E3A8A]/10">
-                    Explore Features
+                <Button size="lg" variant="outline" className="border-[#1E3A8A] text-[#1E3A8A] hover:bg-[#1E3A8A]/10" asChild>
+                    <Link href={'/builder'}>Explore Features</Link>                    
                 </Button>
                 </div>
             </div>
