025f8799ae764285ced02977f651d429512db582
The White Hat KE <austinkings30@gmail.com>
2025-04-20 01:10:00 +0300

    Update import path for navbar in templates page

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import Navbar from "@/components/ui/layout/navbar";
+import Navbar from "@/components/layout/navbar";
 import { TemplatesList } from "@/components/TemplateSelector";
 import { ResumeData, ResumeTemplate } from "../../lib/types";
 
