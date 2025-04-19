41409f881b844e1c9bc8b161ddfb835b5eb91446
The White Hat KE <austinkings30@gmail.com>
2025-04-20 00:08:21 +0300

    Add theme provider wrapper and them switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {/* <AuthProvider> */}
+          {children}
+          <Toaster />
+        {/* </AuthProvider> */}
+      </ThemeProvider>
       </body>
     </html>
   );
