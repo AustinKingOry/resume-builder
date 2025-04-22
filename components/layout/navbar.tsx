138fec0b4ec4aae2c8cc42c5e090530da901de9c
The White Hat KE <austinkings30@gmail.com>
2025-04-22 23:46:17 +0300

    Add dynamic nav links for movile view

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import Link from "next/link";
-import { LogIn } from 'lucide-react';
+import { LogIn, MenuIcon } from 'lucide-react';
 import { appData } from "@/lib/data";
 import Image from "next/image";
 import { ThemeSwitcher } from "../theme-switcher";
 
 const Navbar: React.FC = () => {
+	const links = [
+		{title:"Home",link:"/"},
+		{title:"Resume Builder",link:"/builder"},
+		{title:"Templates",link:"/templates"},
+		{title:"Cover Photos",link:"/coverphotos"},
+	]
 	return (
 		<header className="border-b">
 		<div className="container mx-auto flex h-16 items-center justify-between px-4">
+			<Button type="button" className="flex md:hidden">
+				<MenuIcon className="w-4 h-4" />
+			</Button>
 			<Link href="/" className="flex items-center space-x-2">
 				<Image src="/logo.png?height=56&width=80" width={100} height={56} alt="KaziKit Logo" />
 				<span className="text-xl font-bold sr-only">{appData.name}</span>
 			</Link>
 			<nav className="hidden md:flex items-center space-x-6">
-			<Link href="/" className="text-sm font-medium transition-colors hover:text-primary">
-				Home
-			</Link>
-			<Link href="/builder" className="text-sm font-medium transition-colors hover:text-primary">
-				Resume Builder
-			</Link>
-			<Link href="/templates" className="text-sm font-medium transition-colors hover:text-primary">
-				Templates
-			</Link>
-			<Link href="/coverphotos" className="text-sm font-medium transition-colors hover:text-primary">
-				Cover Photos
-			</Link>
+				{links.map((item, index)=>(
+				<Link key={index} href={item.link} className="text-sm font-medium transition-colors hover:text-primary">
+				{item.title}
+				</Link>
+				))}
 			</nav>
 			<div className="flex items-center space-x-4">
 			<Link href="/login">
@@ -39,6 +41,15 @@ const Navbar: React.FC = () => {
 				<Button>Create Resume</Button>
 			</Link>
 			<ThemeSwitcher />
+			</div>
+		</div>
+		<div className="w-full">
+			<div className="flex flex-col">
+				{links.map((item, index)=>(
+				<Link key={index} href={item.link} className="text-sm font-medium transition-colors hover:text-primary">
+				{item.title}
+				</Link>
+				))}
 			</div>
 		</div>
 		</header>
