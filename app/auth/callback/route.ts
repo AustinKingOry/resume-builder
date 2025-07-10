fc99e985426a96b30afd32884afdf3eb49d17109
The White Hat KE <austinkings30@gmail.com>
2025-07-10 17:56:34 +0300

    Conditionally create profile when user logs in with Oauth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,15 +4,24 @@ import { redirect } from 'next/navigation';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(request: Request) {
-  const supabase = await createServerClient();
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    const supabase = await createServerClient();
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
 
-  if (session) {
-    // Optionally: store user in DB
-    return redirect('/builder');
-  } else {
-    return redirect('/login');
-  }
+    if (session) {
+        // Optionally: store user in DB
+        const { data: profile } = await supabase.from("profiles").select("*").eq("user_id", session.user?.id).single();
+        // Create initial profile record
+        if(!profile){
+            await supabase.from("profiles").insert({
+                user_id: session.user.id,
+                full_name: session.user.user_metadata.full_name,
+                email: session.user.email,
+            })
+        }
+        return redirect('/builder');
+    } else {
+        return redirect('/login');
+    }
 }
