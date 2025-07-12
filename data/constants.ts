f739bcf7a685c0ee0d80d22123b7c2050d314e1c
The White Hat KE <austinkings30@gmail.com>
2025-07-12 16:49:41 +0300

    Add constants data

---
+++ data/constants.ts
@@ -0,0 +1,11 @@
+export const SiteInfo = {
+    name: "Kazikit",
+    email: "kazikit.tools@gmail.com",
+    twitter: "https://x.com/KaziKit_",
+    instagram: "",
+    facebook: "",
+    logo: "/logo.png",
+    description: "Your all-in-one toolkit to build a standout professional profile and thrive in your career journey.",
+    tagline: "Build. Stand Out.",
+    oneLiner: "An all-in-one digital toolkit that gives the African youth the tools to compete globally.",
+}
