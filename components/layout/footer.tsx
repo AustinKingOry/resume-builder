bc0c60594499ccf6ed0b2fd3ecabadcb319fa46a
The White Hat KE <austinkings30@gmail.com>
2025-07-12 16:58:28 +0300

    Update footer data to use constants

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,3 +1,4 @@
+import { SiteInfo } from "@/data/constants"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -9,10 +10,10 @@ export function Footer() {
                 <div className="flex flex-col space-y-4">
                     <div className="flex items-center gap-2">
                     <Image src="/logo.png?height=56&width=100" width={100} height={56} alt="KaziKit Logo" />
-                    <span className="text-xl font-bold text-[#1E3A8A] sr-only">KaziKit</span>
+                    <span className="text-xl font-bold text-[#1E3A8A] sr-only">{SiteInfo.name}</span>
                     </div>
                     <p className="text-sm text-gray-600 dark:text-muted-foreground">
-                    Your all-in-one toolkit to build a standout professional profile and thrive in your career journey.
+                    {SiteInfo.description}
                     </p>
                 </div>
                 <div className="flex flex-col space-y-4">
@@ -51,7 +52,7 @@ export function Footer() {
                 <div className="flex flex-col space-y-4">
                     <h3 className="text-lg font-bold text-[#1E3A8A] dark:text-blue-600">Connect</h3>
                     <div className="flex space-x-4">
-                    <Link href="#" className="text-gray-600 hover:text-[#1E3A8A]">
+                    <Link target="_blank" href={SiteInfo.facebook || "#"} className="text-gray-600 hover:text-[#1E3A8A]">
                         <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                         <path
                             fillRule="evenodd"
@@ -60,12 +61,12 @@ export function Footer() {
                         />
                         </svg>
                     </Link>
-                    <Link href="#" className="text-gray-600 hover:text-[#1E3A8A]">
+                    <a target="_blank" href={SiteInfo.twitter || "#"} className="text-gray-600 hover:text-[#1E3A8A]">
                         <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                         <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                         </svg>
-                    </Link>
-                    <Link href="#" className="text-gray-600 hover:text-[#1E3A8A]">
+                    </a>
+                    <Link target="_blank" href={SiteInfo.instagram || "#"} className="text-gray-600 hover:text-[#1E3A8A]">
                         <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                         <path
                             fillRule="evenodd"
@@ -80,7 +81,7 @@ export function Footer() {
                 </div>
                 <div className="mt-8 border-t pt-8">
                 <p className="text-center text-sm text-gray-600 dark:text-muted-foreground">
-                    &copy; {new Date().getFullYear()} KaziKit. All rights reserved.
+                    &copy; {new Date().getFullYear()} {SiteInfo.name}. All rights reserved.
                 </p>
                 </div>
             </div>
