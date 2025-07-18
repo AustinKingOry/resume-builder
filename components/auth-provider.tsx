4e3918e022c17b5f62f9226c15a82162f83b8484
The White Hat KE <austinkings30@gmail.com>
2025-07-18 15:38:23 +0300

    Add auth callback to redirect to custom callback

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -142,7 +142,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 			const { error } = await supabase.auth.signInWithOAuth({
 				provider: "google",
 				options: {
-				  redirectTo: `https://kazikit.vercel.app`,
+				  redirectTo: `https://kazikit.vercel.app/auth/callback`,
 				}
 			  });
 			if (error) {
