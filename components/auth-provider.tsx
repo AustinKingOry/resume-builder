868dc4933e49f00a092d2c4afc93f26bbf92627f
The White Hat KE <austinkings30@gmail.com>
2025-07-21 17:32:54 +0300

    refactor: change callback url for google signin

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -142,7 +142,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 			const { error } = await supabase.auth.signInWithOAuth({
 				provider: "google",
 				options: {
-				  redirectTo: `https://kazikit.vercel.app/auth/callback`,
+				  redirectTo: `https://kazikit.vercel.app`,
 				}
 			  });
 			if (error) {
