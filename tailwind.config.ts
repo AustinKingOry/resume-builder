4a8d7eede016343deb8dda888a4e51653587194c
austinkings30 <austinkings30@gmail.com>
2025-08-07 15:09:11 +0000

    chore: add shadcn ui sidebar component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -64,7 +64,17 @@ export default {
 					'orange': '#C05621',
 					'blue': '#2B6CB0',
 					'red': '#C53030'
-				}
+				},
+				sidebar: {
+					DEFAULT: 'hsl(var(--sidebar-background))',
+					foreground: 'hsl(var(--sidebar-foreground))',
+					primary: 'hsl(var(--sidebar-primary))',
+					'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+					accent: 'hsl(var(--sidebar-accent))',
+					'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+					border: 'hsl(var(--sidebar-border))',
+					ring: 'hsl(var(--sidebar-ring))'
+				},
 			},
   		borderRadius: {
   			lg: 'var(--radius)',
