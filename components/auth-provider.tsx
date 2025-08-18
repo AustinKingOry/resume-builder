9489d4a049608750fa64f461f29c3896bad599f1
austinkings30 <austinkings30@gmail.com>
2025-08-18 17:40:49 +0000

    change google auth redirect url

--- components/auth-provider.tsx
+++ components/auth-provider.tsx
@@ -142,7 +142,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 			const { error } = await supabase.auth.signInWithOAuth({
 				provider: "google",
 				options: {
-				  redirectTo: `https://kazikit.vercel.app/roast-my-cv`,
+				  redirectTo: `https://kazikit.vercel.app`,
 				}
 			  });
 			if (error) {
